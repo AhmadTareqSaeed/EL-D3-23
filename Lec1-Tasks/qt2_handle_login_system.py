7061a265748539381f50f1d1f9190da35f1dc41c
AhmadTareqSaeed <ahmatareeq@gmail.com>
2023-08-09 03:30:15 +0300

    qt2

--- Lec1-Tasks/qt2_handle_login_system.py
+++ Lec1-Tasks/qt2_handle_login_system.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-users = {'Ahmad': '1394', 'Ali': '6078', 'Amr': '9345'}  
+users = {'Ahmad': '1394',
+	 'Ali'  : '6078',
+	 'Amr'  : '9345'}  
    
 username = input("Please Enter a username: ")  
 password = input("Please Enter a password: ")  
