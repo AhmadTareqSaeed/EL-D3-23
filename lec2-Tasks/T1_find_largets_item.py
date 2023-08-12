a6d227cb94b019aff54106b33449f9951701551b
AhmadTareqSaeed <ahmatareeq@gmail.com>
2023-08-12 05:53:52 +0300

    Edit for task1

--- lec2-Tasks/T1_find_largets_item.py
+++ lec2-Tasks/T1_find_largets_item.py
@@ -1,9 +1,6 @@
 #Find the largest item from a given list using loop
 
 nums = [21,123,14,90,120,11]
-lnum = -1
-for i in nums:
-    if i > lnum:
-        lnum = i
-print("largest number is :",lnum)
+nums.sort()
+print("largest number is :",nums[-1])
 
